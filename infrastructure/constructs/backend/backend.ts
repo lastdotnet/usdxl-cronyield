d75e3691822cf529305b3d9ec2a687f96f2f78f7
Mike Albanese <alphak3y@protonmail.com>
2025-06-04 11:14:53 -0700

    set cron to 8am utc

--- infrastructure/constructs/backend/backend.ts
+++ infrastructure/constructs/backend/backend.ts
@@ -118,8 +118,8 @@ export class Backend extends Construct {
         },
         // Run once per day at 12:00 UTC
         schedule: events.Schedule.cron({
-          minute: "11",
-          hour: "18",
+          minute: "00",
+          hour: "08",
           day: "*",
           month: "*",
           year: "*",
